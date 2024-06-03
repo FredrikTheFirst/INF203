4492e19fc2e468f62512a41322adfa5a175da350
Fredrik G Haugen <fredrik.gard.haugen@nmbu.no>
2024-06-03 14:26:41 +0200

    er ikke ferdig med den fundamentale infrastrukturen

---
+++ mesh_object.py
@@ -0,0 +1,31 @@
+import meshio
+
+
+class line_cell:
+    def __ init__(self):
+        pass
+
+class triangle_cell:
+    def __init__(self):
+        pass
+
+
+class cell_factory:
+    def __init__(self) -> None:
+        self._cell_types = {}
+    
+    def register(self, key, name):
+        self._cell_types[key] = name
+
+class mesh():
+    def __init__(self, mesh_file):
+        factory = cell_factory()
+        factory.register()
+
+
+
+
+
+mesh_file = 'bay.msh'  # Going to test the classes mesh, triangle_cell and line_cell with "bay.mesh" 
+
+msh = mesh(mesh_file)
