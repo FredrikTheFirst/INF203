7c3f3c06107fa3a369a40253346253554efaa814
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2024-06-04 15:19:21 +0200

    added nvectors func

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 def v(x):
     return [x[1] - 0.2*x[0], -x[0]]
+
 
 def g(a, b, v, u):
     dot = np.array(v) @ np.array(u)
@@ -9,17 +11,21 @@ def g(a, b, v, u):
         return a * dot
     else:
         return b * dot
-    
+
+
 def A(coords):
-    a, b, c = np.array(coords[0][0:2]), np.array(coords[1][0:2]), np.array(coords[2][0:2])
+    a, b, c = np.array(coords[0]), np.array(coords[1]), np.array(coords[2])
     v1 = b - a
     v2 = c - a
     return 0.5 * abs(np.cross(v1, v2))
 
+
 def midpoint(coords):
     return 1/3 * sum(coords)
 
+
 M = np.array([[0, -1], [1, 0]])
+
 
 def nvector(v1, v2, mid):
     e = v2 - v1
@@ -30,8 +36,9 @@ def nvector(v1, v2, mid):
     else:
         return -nn
 
-def vvector(v1, v2, mid):
-    e = v2 - v1
+
+def vvector(line, mid):
+    e = line[1] - line[0]
     nn = M @ e
     pl = v2 - mid + v1 - mid
     if nn @ pl > 0:
@@ -40,15 +47,15 @@ def vvector(v1, v2, mid):
         return -nn
 
 
-'''
-def nvectors(coords, mid):
-    a, b, c = np.array(coords[0][0:2]), np.array(coords[1][0:2]), np.array(coords[2][0:2])
-    e1 = b - a
-    e2 = a - c
-    e3 = c - b
-    M = np.array([[0, -1], [1, 0]])
-    n1 = M @ e1 / np.linalg.norm(e1)
-    n2 = M @ e2 / np.linalg.norm(e2)
-    n3 = M @ e3 / np.linalg.norm(e3)
-    return np.array([n1, n2, n3])
-'''
+#IKKE FERDIG
+def nvectors(coords):
+    coords = coords.tolist()
+    print(coords)
+    vectorset = []
+    for i, item in enumerate(coords):
+        pair = [item]
+        coords.pop(i)
+        for vector in coords:
+            pair.append(vector)
+            vectorset.append(pair)
+            pair = [item]
