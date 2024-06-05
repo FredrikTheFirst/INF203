5b4e9ee5c10eec98c02627a4bc2a96d1fd66f014
Fredrik G Haugen <fredrik.gard.haugen@nmbu.no>
2024-06-05 16:24:31 +0200

    Tried makeing the next fram of the simulation

--- starting_img.py
+++ starting_img.py
@@ -16,30 +16,30 @@ vfelt = np.array([fc.v(tri._midpoint) for tri in msh._triangles])
 Afelt = np.array([tri._area for tri in msh._triangles])
 #nufelt = np.array([fc.morevectors(np.array(), fc.nuvector) for neighbour in tri._neighbours] for tri in msh._triangles])
 
-dt = 0.5
+dt = 0.05
 
 u = np.array([fc.starting_amount(x_mid, tri._midpoint) for tri in msh._triangles])
-ungh = np.array([[fc.starting_amount(x_mid, mid) for mid in tri._neighbours] for tri in msh._triangles])
+# ungh = np.array([[fc.starting_amount(x_mid, msh._triangles[int(neigh)]._midpoint) for neigh in tri._neighbours] for tri in msh._triangles])
 Fsumlist = []
 for tri in msh._triangles:
     u_old = u[tri._cell_id]
     tri_v = vfelt[tri._cell_id]
     Flist = []
     for neigh in tri._neighbours:
-        neigh = msh._triangles[neigh]
+        neigh = msh._triangles[int(neigh)]
         print(tri._points)
         print(neigh._points)
         matching_points = set(tri._points) & set(neigh._points)
         print(matching_points)
-        matching_coords = np.array(msh._points[matching_points])
+        matching_coords = np.array([msh._points[point] for point in matching_points])
         nu = fc.nuvector(matching_coords[0], matching_coords[1], tri._midpoint)
         u_old_neigh = u[neigh._cell_id]
-        F = fc.dOil(dt, tri._area, g((u_old, u_old_neigh, tri_v, nu)))
+        F = fc.dOil(dt, tri._area, fc.g(u_old, u_old_neigh, tri_v, nu))
         Flist.append(F)
     sum(Flist)
     Fsumlist.append(Flist)
 
-u2 = np.array([sum(e) for e in zip(u, Fsumlist)])
+u2 = np.array([u_old + np.sum(flux) for u_old, flux in zip(u, Fsumlist)])
 
 
 
@@ -51,35 +51,37 @@ u2 = np.array([sum(e) for e in zip(u, Fsumlist)])
 # triangle2 = np.array([[0, 0], [1, 1], [1, 0]])
 
 # Plot the mesh by adding all triangles with their value
-plt.figure()
+
+for i, el in enumerate((u, u2)):
+    plt.figure()
 
 
-# Create the colormap
-sm = plt.cm.ScalarMappable(cmap='viridis')
-sm.set_array([u])
-umax = max(u)
-umin = min(u)
+    # Create the colormap
+    sm = plt.cm.ScalarMappable(cmap='viridis')
+    sm.set_array([el])
+    umax = max(el)
+    umin = min(el)
 
-# Add colorbar using a separate axis
-cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1]) # adjust position and size as needed
-plt.colorbar(sm, cax=cbar_ax, label='label3')
+    # Add colorbar using a separate axis
+    cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1]) # adjust position and size as needed
+    plt.colorbar(sm, cax=cbar_ax, label='label3')
 
-# Need for-loop
-for tri, amount in zip(msh._triangles, u):
-    coords = msh._points[tri._points]
-    plt.gca().add_patch(plt.Polygon(coords, color=plt.cm.viridis((amount - umin)/(umax - umin)), alpha=0.9))
+    # Need for-loop
+    for tri, amount in zip(msh._triangles, el):
+        coords = msh._points[tri._points]
+        plt.gca().add_patch(plt.Polygon(coords, color=plt.cm.viridis((amount - umin)/(umax - umin)), alpha=0.9))
 
 
-# plt.gca().add_patch(plt.Polygon(triangle1, color=plt.cm.viridis((u[0] - umin)/(umax - umin)), alpha=0.9))
-# plt.gca().add_patch(plt.Polygon(triangle2, color=plt.cm.viridis((u[1] - umin)/(umax - umin)), alpha=0.9))
+    # plt.gca().add_patch(plt.Polygon(triangle1, color=plt.cm.viridis((u[0] - umin)/(umax - umin)), alpha=0.9))
+    # plt.gca().add_patch(plt.Polygon(triangle2, color=plt.cm.viridis((u[1] - umin)/(umax - umin)), alpha=0.9))
 
-# Add labels to axes
-plt.xlabel('label1')
-plt.ylabel('label2')
-plt.xlim(0, 1) # set the x-axis limits
-plt.ylim(0, 1) # set the y-axis limits
-plt.gca().set_aspect('equal')
+    # Add labels to axes
+    plt.xlabel('label1')
+    plt.ylabel('label2')
+    plt.xlim(0, 1) # set the x-axis limits
+    plt.ylim(0, 1) # set the y-axis limits
+    plt.gca().set_aspect('equal')
 
-# Show plot
-plt.savefig("start_img.png")
-plt.close()
+    # Show plot
+    plt.savefig(f"tmp\start_img_{i}.png")
+    plt.close()
