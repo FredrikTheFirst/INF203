901566e8b1847a85805d0b1015ce0a8d5f558bde
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2024-06-05 11:25:26 +0200

    tested in testfile

--- testfile.py
+++ testfile.py
@@ -1,11 +1,16 @@
-from mesh_object import line_cell, triangle_cell, cell_factory, mesh
+from mesh_object import mesh
+import numpy as np
 
 file_name = 'bay.msh'
 test = mesh(file_name)
-
 
 
 for tri in test._triangles:
     print(type(tri._points))
     print(tri._points)
     print(tri._cell_id)
+    crds = np.array([test._points[i] for i in tri._points.tolist()])
+    print(crds)
+    tri.find_area(crds)
+    print(tri._area)
+    print()
