b05fe8df3b669dac6734e726e08f34b9c262274e
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2024-06-07 14:36:52 +0200

    simulation class finished

--- simulation.py
+++ simulation.py
@@ -2,16 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from functions import *
 from mesh_object import *
+import cv2
 
 class Simulation():
-    def __init__(self, filename):
+    def __init__(self, filename, midpoint = np.array([0.35, 0.45])):
         self._filename = filename
-        self.x_mid = np.array([0.35, 0.45])
+        self.x_mid = midpoint
         self.vfelt = np.array([v(cell.midpoint) for cell in msh.cells])
         self.u = np.array([starting_amount(self.x_mid, cell.midpoint) for cell in msh.cells])
 
-    def runsim(self, dt):
-        self.dt = dt
+    def runsim(self, frames = 500, time = 0.5):
+        self.frames = frames
+        self.dt = time/frames
         Oillist = self.u.copy()
         for tri in msh.get_triangles():
             u_old = self.u[tri.id]
@@ -34,12 +36,61 @@ class Simulation():
         #return np.array(Oillist)
         self.Oillist = Oillist
 
-    def photo(self):
-        pass
+    def photo(self, i):
+        el = self.Oillist[i]
+        
+        plt.figure()
 
-    def photos(self):
-        pass
+        # Create the colormap
+        sm = plt.cm.ScalarMappable(cmap='viridis')
+        sm.set_array([el])
+        umax = max(el)
+        umin = min(el)
+
+        # Add colorbar using a separate axis
+        cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1]) # adjust position and size as needed
+        plt.colorbar(sm, cax=cbar_ax, label='label3')
+
+        # Need for-loop
+        for cell, amount in zip(msh.cells, el):
+            coords = msh.coords[cell.points]
+            plt.gca().add_patch(plt.Polygon(coords, color=plt.cm.viridis((amount - umin)/(umax - umin)), alpha=0.9))
+
+
+        # plt.gca().add_patch(plt.Polygon(triangle1, color=plt.cm.viridis((u[0] - umin)/(umax - umin)), alpha=0.9))
+        # plt.gca().add_patch(plt.Polygon(triangle2, color=plt.cm.viridis((u[1] - umin)/(umax - umin)), alpha=0.9))
+
+        # Add labels to axes
+        plt.xlabel('label1')
+        plt.ylabel('label2')
+        plt.xlim(0, 1) # set the x-axis limits
+        plt.ylim(0, 1) # set the y-axis limits
+        plt.gca().set_aspect('equal')
+
+        # Show plot
+        plt.savefig(f"tmp\start_img_{i}.png")
+        plt.close()
+
+    def photos(self, intv):
+        for i in range(0, self.frames, intv)
+            self.photo(i)
 
     def makevideo(self):
-        pass
+
+        # Get the list of image files in the directory
+        images = [f"tmp/start_img_{i}.png" for i in range(0,25)]
+
+        # determine dimension from first image
+        frame = cv2.imread(images[0])
+        height, width, layers = frame.shape
+
+        ## Define the codec and create a VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v') # or 'XVID', 'DIVX', 'mp4v' etc.
+        video = cv2.VideoWriter("video.AVI", fourcc, 5, (width, height)) # 5 frames per second
+
+        for image in images:
+            video.write(cv2.imread(image))
+
+        cv2.destroyAllWindows()
+        video.release()
 
