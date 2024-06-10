52d8f34a7e240b897fba2f496a85fe4492813e2a
Fredrik G Haugen <fredrik.gard.haugen@nmbu.no>
2024-06-10 09:48:30 +0200

    changed alot with tests

--- test_cellChild.py
+++ test_cellChild.py
@@ -1,12 +1,11 @@
 import pytest
-from src.package import Triangle_cell, Line_cell
+from src.package.cellChild import Triangle_cell, Line_cell
 
 
-@pytest.mark.parametrize('id_list', [i for i in range(13)])
-def test_id(id_list):
-    for id in id_list:
-        a = Line_cell(id, [])
-        b = Triangle_cell(id, [])
-        assert a.id == id
-        assert b.id == id
+@pytest.mark.parametrize('id', [1, 6, 29])
+def test_id(id):
+    a = Line_cell(id, [])
+    b = Triangle_cell(id, [])
+    assert a.id == id, 'Store id'
+    assert b.id == id, 'Store id'
 
--- tests/test_cellChild.py
+++ tests/test_cellChild.py
@@ -1,11 +1,13 @@
 import pytest
-from src.package import *
+from src.package.cellChild import *
 
 
-@pytest.mark.parametrize('id', [i for i in range(13)])
+@pytest.mark.parametrize('id', [1, 6, 29])
 def test_id(id):
     a = Line_cell(id, [])
     b = Triangle_cell(id, [])
-    assert a.id == 2, 'Problem with Line_cell`s id-attribute'
+    assert a.id == id, 'Problem with Line_cell`s id-attribute'
     assert b.id == id, 'Problem with Triangel_cell`s id-attribute'
+
+
 
